25e5969f570ae154d7e2d1fc22fb08c25ea81939
trane7776 <izbasarov.t@edu.rea.ru>
2023-08-22 13:12:35 +0300

    updated front

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -48,15 +48,18 @@ const Customizer = () => {
     if (!prompt) return alert('Please enter a prompt');
     try {
       setGeneratingImg(true);
-      const response = await fetch('http://localhost:8080/api/v1/dalle', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          prompt,
-        }),
-      });
+      const response = await fetch(
+        'https://threejs-app-j7w9.onrender.com/api/v1/dalle',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            prompt,
+          }),
+        }
+      );
       const data = await response.json();
       if (data.photo === undefined) {
         alert('Failed to generate image. Try again.');
