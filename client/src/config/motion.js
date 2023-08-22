5560e7a76dae539a0003610be508c36a99d4b531
trane7776 <izbasarov.t@edu.rea.ru>
2023-08-22 12:19:01 +0300

    first commit

---
+++ client/src/config/motion.js
@@ -0,0 +1,85 @@
+export const transition = { type: "spring", duration: 0.8 };
+
+export const slideAnimation = (direction) => {
+  const transition = { type: "spring", duration: 0.8, delay: 0 };
+
+  const getPosition = (direction) => {
+    switch (direction) {
+      case "left":
+        return { x: -100, y: 0 };
+      case "right":
+        return { x: 100, y: 0 };
+      case "up":
+        return { x: 0, y: 100 };
+      case "down":
+        return { x: 0, y: -100 };
+      default:
+        return { x: 0, y: 0 };
+    }
+  };
+
+  return {
+    initial: {
+      ...getPosition(direction),
+      opacity: 0,
+      transition: { ...transition, delay: 0.5 },
+    },
+    animate: {
+      x: 0,
+      y: 0,
+      opacity: 1,
+      transition: { ...transition, delay: 0 },
+    },
+    exit: {
+      ...getPosition(direction),
+      transition: { ...transition, delay: 0 },
+    },
+  };
+};
+
+export const fadeAnimation = {
+  initial: {
+    opacity: 0,
+    transition: { ...transition, delay: 0.5 },
+  },
+  animate: {
+    opacity: 1,
+    transition: { ...transition, delay: 0 },
+  },
+  exit: {
+    opacity: 0,
+    transition: { ...transition, delay: 0 },
+  },
+};
+
+export const headTextAnimation = {
+  initial: { x: 100, opacity: 0 },
+  animate: { x: 0, opacity: 1 },
+  transition: {
+    type: "spring",
+    damping: 5,
+    stiffness: 40,
+    restDelta: 0.001,
+    duration: 0.3,
+  },
+};
+
+export const headContentAnimation = {
+  initial: { y: 100, opacity: 0 },
+  animate: { y: 0, opacity: 1 },
+  transition: {
+    type: "spring",
+    damping: 7,
+    stiffness: 30,
+    restDelta: 0.001,
+    duration: 0.6,
+    delay: 0.2,
+    delayChildren: 0.2,
+  },
+};
+
+export const headContainerAnimation = {
+  initial: { x: -100, opacity: 0, transition: { ...transition, delay: 0.5 } },
+  animate: { x: 0, opacity: 1, transition: { ...transition, delay: 0 } },
+  exit: { x: -100, opacity: 0, transition: { ...transition, delay: 0 } },
+};
---
+++ server/routes/dalle.routes.js
@@ -0,0 +1,53 @@
+import express from 'express';
+
+import * as dotenv from 'dotenv';
+import Replicate from 'replicate';
+import { reader } from '../../client/src/config/helpers.js';
+import axios from 'axios';
+import fs from 'fs';
+dotenv.config();
+
+const router = express.Router();
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN,
+});
+
+router.route('/').get((req, res) => {
+  res.status(200).json({ message: 'Hello World Routes' });
+});
+
+router.route('/').post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+    const model =
+      'stability-ai/stable-diffusion:27b93a2413e7f36cd83da926f3656280b2931564ff050bf9575f1fdf9bcd7478';
+    const input = {
+      prompt,
+      width: 384,
+      height: 384,
+    };
+    const output = await replicate.run(model, { input });
+    convertImageToBase64Json(output[0])
+      .then((base64Json) => {
+        res.status(200).json({ photo: base64Json });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Something went wrong' });
+  }
+});
+async function convertImageToBase64Json(url) {
+  try {
+    const response = await axios.get(url, { responseType: 'arraybuffer' });
+    const imageBuffer = await Buffer.from(response.data, 'binary');
+    const base64Image = await imageBuffer.toString('base64');
+    return base64Image;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to convert image to Base64 JSON');
+  }
+}
+export default router;
