032e732be9f8b09afcf18e20666159dd4ccf1ec9
trane7776 <izbasarov.t@edu.rea.ru>
2023-08-22 15:42:21 +0300

    update fetch 2

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -52,9 +52,6 @@ const Customizer = () => {
         'https://threejs-shirtapp.onrender.com/api/v1/dalle',
         {
           method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
           body: JSON.stringify({
             prompt,
           }),
