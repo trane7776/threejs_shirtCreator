21c2c30b2cb5aecd7286029eccc139e0a49b1c31
trane7776 <izbasarov.t@edu.rea.ru>
2023-08-22 15:57:03 +0300

    added typescript dependency

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -13,8 +13,7 @@ const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN,
   fetch: fetch,
 });
-const headers = new Headers();
-headers.append('Content-Type', 'application/json');
+
 router.route('/').get((req, res) => {
   res.status(200).json({ message: 'Hello World Routes' });
 });
